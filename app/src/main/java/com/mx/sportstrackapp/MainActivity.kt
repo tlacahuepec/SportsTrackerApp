6c91ee3cfc6be196d4cfc5e05be75cc75fe66b18
Edward Santiesteban <Edward.Santiesteban@aa.com>
2020-09-03 16:45:33 -0500

    initial commit

---
+++ app/src/main/java/com/mx/sportstrackapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mx.sportstrackapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mx.sportstrackapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
