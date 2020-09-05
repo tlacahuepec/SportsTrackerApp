7f3774d1a94f1e079c058f9a9ab60f6b0c78cf45
Edward Santiesteban <Edward.Santiesteban@aa.com>
2020-09-05 09:18:24 -0500

    Removed unused code, added basic components

--- app/src/main/java/com/mx/sportstrackapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/mx/sportstrackapp/ui/main/MainFragment.kt
@@ -10,10 +10,6 @@ import com.mx.sportstrackapp.R
 
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
