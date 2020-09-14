e24353e63f5970769fec24e822f780431fe97123
Edward Santiesteban <Edward.Santiesteban@aa.com>
2020-09-14 10:03:43 -0500

    Progress Bar now Working

--- app/src/main/java/com/mx/sportstrackapp/ui/main/BindingAdapters.kt
+++ app/src/main/java/com/mx/sportstrackapp/ui/main/BindingAdapters.kt
@@ -1,7 +1,10 @@
 package com.mx.sportstrackapp.ui.main
 
+import android.view.View
+import android.widget.ProgressBar
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.mx.sportstrackapp.search.api.SportsServiceStatus
 import com.mx.sportstrackapp.search.api.data.Team
 import com.mx.sportstrackapp.ui.main.adapter.TeamResultsAdapter
 
@@ -9,4 +12,14 @@ import com.mx.sportstrackapp.ui.main.adapter.TeamResultsAdapter
 fun bindRecyclerView(recyclerView: RecyclerView, teamList: List<Team>?) {
     val adapter = recyclerView.adapter as TeamResultsAdapter
     adapter.submitList(teamList)
+}
+
+@BindingAdapter("sportsServiceStatus")
+fun bindSportsServiceStatus(progressBar: ProgressBar, status: SportsServiceStatus?) {
+    when (status) {
+        SportsServiceStatus.LOADING -> progressBar.visibility = View.VISIBLE
+        SportsServiceStatus.ERROR -> progressBar.visibility = View.GONE
+        SportsServiceStatus.DONE -> progressBar.visibility = View.GONE
+        null -> progressBar.visibility = View.GONE
+    }
 }
